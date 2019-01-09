790f9c866e9928bb0bc820aa2f49481c3c2c51f6
Sam Smedinghoff <smeds1@gmail.com>
2019-01-09 12:29:42 -0800

    worked on chat history

--- project2/Static/index.js
+++ project2/Static/index.js
@@ -1,12 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.querySelectorAll('a').forEach(a => {
-          a.onclick = () => {
-              document.querySelector('#chatentry').disabled = false;
-              document.querySelector('#channel_name').innerHTML = `Channel: ${a.innerHTML}`;
-          };
-      });
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+  socket.on('connect', () => {
+
+        //load chat history when channel is clicked
+        document.querySelectorAll('a').forEach(a => {
+            a.onclick = () => {
+              const channel = a.innerHTML;
+              document.querySelector('#chatentry').disabled = false;
+              document.querySelector('#channel_name').innerHTML = channel;
+              socket.emit('pick channel', {'channel': channel});
+            }
+        });
+
+        //update chat history when chat is entered
+        document.querySelector('#chatentry').onblur = () => {
+            const text = document.querySelector('#chatentry').value;
+            const channel = document.querySelector('#channel_name').innerHTML;
+            socket.emit('enter chat', {'text': text, 'channel': channel});
+        };
+    });
+
+    // display all previous chats on the selected channel
+    socket.on('display_previous_chats', chats => {
+          chats.forEach(c => {
+            const li = document.createElement('li');
+            li.innerHTML = `${c}`;
+            document.querySelector('#previous_chats').append(li);
+          })
+    });
+
+    // When a new chat is entered, add to the unordered list
+    socket.on('update_chat', data => {
+        const li = document.createElement('li');
+        li.innerHTML = `${data.user} (${data.time}): ${data.text}`;
+        document.querySelector('#previous_chats').append(li);
+    });
+
+    // Create a new channel
     document.querySelector('#new_channel').onsubmit = () => {
 
         // Initialize new request
--- project2/application.py
+++ project2/application.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify, session, render_template, request
 from flask_session import Session
 from flask_socketio import SocketIO, emit
+import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -13,20 +14,37 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 channels = []
+chats = {}
 
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == 'POST':
         session["user"] = request.form.get("uname")
-        #return render_template("index.html",user=session.get("user"))
     return render_template("index.html",user=session.get("user"),channels=channels)
 
 @app.route("/new_channel", methods=["POST"])
 def new_channel():
-
     channel = request.form.get("channel")
     if channel in channels:
         return jsonify({"success": False})
     else:
         channels.append(channel)
+        chats[channel] = []
         return jsonify({"success": True, "channel": channel})
+
+@socketio.on("pick channel")
+def pick_channel(data):
+    channel = data["channel"]
+    emit("display_previous_chats", chats[channel], broadcast=True)
+
+@socketio.on("enter chat")
+def enter_chat(data):
+    text = data["text"]
+    channel = data["channel"]
+    if len(chats[channel]) < 100:
+        chats[channel].append(text)
+    else:
+        chats[channel] = chats[channel][1:] + [text]
+    timestamp = datetime.datetime.now()
+    time = "{}-{}-{}".format(int(timestamp.month),int(timestamp.day),timestamp.year)
+    emit("update_chat", {'text':text, 'user':session['user'], 'time':time}, broadcast=True)
